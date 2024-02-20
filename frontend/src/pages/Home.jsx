2242c77424b5678cd725bf83de88d326d226afcf
Shivam1320 <shivam.skt13@gmail.com>
2024-02-20 18:28:32 +0530

    added the About section page and makes some changes in Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,60 +1,63 @@
-import React from 'react'
-
 import heroImg01 from '../assets/images/hero-img01.png';
 import heroImg02 from '../assets/images/hero-img02.png';
 import heroImg03 from '../assets/images/hero-img03.png';
 import icon01 from '../assets/images/icon01.png';
-// import { Link } from 'react-router-dom';
-// import {BsRightArrow} from 'react-icons/bs';
+import icon02 from '../assets/images/icon02.png';
+import icon03 from '../assets/images/icon03.png';
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
+import About from '../components/About/About';
+
 const Home = () => { 
-  return(
-     <>
-  {/* ----hero section ----- */}
+  return (
+    <>
   
-  <section className='hero_section pt-[60px] 2xl : h-[800px]'>
+  {/* ------------ hero section --------------- */}
+  <section className='hero__section pt-[60px] 2xl:h-[800px]'>
     <div className="container">
       <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
-        {/* ----- hero content ------ */}
+        {/* -------------- hero content ---------------- */}
         <div>
-          <div className='lg:w-[570px]'>
+          <div className="lg:w-[570px]">
             <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px]
-            md:leading-[70px]"> We help patients live a healthy, longer life.</h1>
+            md:leading-[70px]"> We help patients live a healthy, longer life.
+            </h1>
             <p className="text__para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi,
              iure possimus odio eveniet sequi animi debitis quibusdam enim nemo dolorem nostrum officia 
              perspiciatis sint eius tenetur, amet accusantium error odit? 
             </p>
             <button className='btn'> Request an Appointment</button>
           </div>
-          {/* ------ hero counter ---- */}
+
+          {/* ---------------- hero counter ------------- */}
           <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5
           lg:gap-[30px]'>
             <div>
               <h2 className='text -[36px] leading-[56px] lg:text-[44px] lg:leading-[54px]
               font-[700] text-headingColor'> 30+</h2>
               <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
-              <p className='text_para'> Years of Experience </p>
+              <p className='text__para'> Years of Experience </p>
             </div>
 
             <div>
               <h2 className='text -[36px] leading-[56px] lg:text-[44px] lg:leading-[54px]
               font-[700] text-headingColor'> 15+</h2>
               <span className='w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]'></span>
-              <p className='text_para'> Clinic Location </p>
+              <p className='text__para'> Clinic Location </p>
             </div>
 
             <div>
               <h2 className='text -[36px] leading-[56px] lg:text-[44px] lg:leading-[54px]
               font-[700] text-headingColor'> 100%</h2>
               <span className='w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]'></span>
-              <p className='text_para'> Patient Satisfaction </p>
+              <p className='text__para'> Patient Satisfaction </p>
             </div>
-
           </div>
         </div>
         {/* ----hero content ---- */}
         <div className='flex gap-[30px] justify-end'>
           <div>
-            <img src= {heroImg01} alt="" />
+            <img className='w-full' src= {heroImg01} alt="" />
           </div>
           <div className='mt-[30px]'>
             <img src={heroImg02} alt="" className='w-full mb-[30px]' />
@@ -64,8 +67,9 @@ const Home = () => {
       </div>
     </div>
   </section>
+{/* ----------- hero section ends --------- */}
 
-{/* ------ hero section end */}
+{/* ------------ Medical Services info section starts -------------- */}
   <section>
     <div className='container'>
       <div className='lg:w-[470px] mx-auto'>
@@ -78,8 +82,9 @@ const Home = () => {
         </p>
       </div>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-
-      [55px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+
+        {/* ------------- info card 1 ------------ */}
         <div className='py-[30px] px-5'>
           <div className='flex items-center justify-center'>
             <img src= {icon01} alt='' />
@@ -90,25 +95,74 @@ const Home = () => {
                Find a Doctor
             </h2>
             <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
-            World-class care for everyone. Our health System offers unmatched,
-            expert health care.From the lab to the clinic
+            World-Class care for everyone. Our health System offers unmatched,
+            expert health care. From the lab to the clinic
             </p>
 
-            {/* <Link to ='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-
-            [#181A1E] mt-[30px] mx-auto flex items-center justify-center group
-            hover:bg-primaryColor hover:border-none'>
-              <BsRightArrow/>
+            <Link to ='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-
+            [#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+              <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+            </Link>
+          </div>
+        </div>
 
-            </Link> */}
+         {/* ------------- info card 2 ------------ */}
+        <div className='py-[30px] px-5'>
+          <div className='flex items-center justify-center'>
+            <img src= {icon02} alt='' />
+          </div>
+
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+               Find Location
+            </h2>
+            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+            World-Class care for everyone. Our health System offers unmatched,
+            expert health care. From the lab to the clinic
+            </p>
+
+            <Link to ='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-
+            [#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+              <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+            </Link>
+          </div>
+        </div>
+
+         {/* ------------- info card 3 ------------ */}
+        <div className='py-[30px] px-5'>
+          <div className='flex items-center justify-center'>
+            <img src= {icon03} alt='' />
+          </div>
+
+          <div className='mt-[30px]'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+               Book Appointment
+            </h2>
+            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+            World-Class care for everyone. Our health System offers unmatched,
+            expert health care. From the lab to the clinic
+            </p>
+
+            <Link to ='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-
+            [#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+              <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+            </Link>
           </div>
         </div>
 
       </div>
-
     </div>
   </section>
+{/* ------------ Medical Services info section ends -------------- */}
+
+
+{/* ------------ About section starts -------------- */}
+<About />
+{/* ------------ About section ends -------------- */}
+
+
   </>
   );
 };
 
-export default Home
+export default Home;
