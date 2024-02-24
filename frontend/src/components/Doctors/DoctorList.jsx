51bc8ac99a48716f76c00c57c9180cb4c9d9ebbe
My Name <khyati0783.be21@chitkara.edu.in>
2024-02-24 13:21:37 +0530

    Third commit

---
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import {doctors} from './../../assets/data/doctors';
+
+const DoctorList = () =>{
+    return (
+        <div> DoctorList</div>
+    )
+}
+
+export default DoctorList
