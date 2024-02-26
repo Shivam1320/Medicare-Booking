dc3da471ed6e87295da5fafec536357547abe6ab
Shivam1320 <shivam.skt13@gmail.com>
2024-02-26 21:56:23 +0530

    created Services and Find a Doctor Page

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -75,7 +75,7 @@ const quickLinks03 = [
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className='pb-16 pt-10'>
+    <footer className='pb-16 pt-10 bg-gray-200'>
       <div className="container">
       <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
         <div>
--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -1,4 +1,3 @@
-import React from "react"; 
 import { services } from './../../assets/data/services';
 import ServiceCard from "./ServiceCard";
 
