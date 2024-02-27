e1fb49b232a5bb2d2ca8f872196f89a66223d9a0
My Name <khyati0783.be21@chitkara.edu.in>
2024-02-27 11:10:14 +0530

    fifth commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import icon01 from '../assets/images/icon01.png';
 import icon02 from '../assets/images/icon02.png';
 import icon03 from '../assets/images/icon03.png';
 import featureImg from '../assets/images/feature-img.png';
+import faqImg from '../assets/images/faq-img.png';
 import videoIcon from '../assets/images/video-icon.png';
 import avatarIcon from '../assets/images/avatar-icon.png';
 import { Link } from 'react-router-dom';
@@ -12,6 +13,8 @@ import { BsArrowRight } from 'react-icons/bs';
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
 import DoctorList from '../components/Doctors/DoctorList';
+import FaqList from '../components/Faq/FaqList';
+import Testimonial from '../components/Testimonial/Testimonial';
 
 const Home = () => { 
   return (
@@ -247,6 +250,42 @@ const Home = () => {
   </div>
 </section>
 {/*-------------- our great doctors section ends----------------*/}
+
+{/*------------------------ faq section ----------------*/}
+<section>
+  <div className='container'>
+    <div className='flex justify-between gap-[50px] lg:gap-0'>
+      <div className='w-1/2 hidden md:block'>
+        <img src={faqImg} alt="" />
+      </div>
+
+      <div className='w-full md:w-1/2'>
+        <h2 className='heading'>
+          Most questions by our beloved patients
+        </h2>
+        <FaqList/>
+      </div>
+
+    </div>
+  </div>
+</section>
+{/*------------------ faq section ends------------*/}
+
+{/*---------------testimonial---------*/}
+<section>
+  <div className="container">
+  <div className='xl:w-[470px] mx-auto'>
+        <h2 className='heading text-center'> What our patient say</h2>
+        <p className='text__para text-center'>
+          World-class care for everyone. Our health system offers unmatched,
+          expert health care.
+        </p>
+    </div>
+    <Testimonial/>
+  </div>
+</section>
+{/*---------------testimonial end---------*/}
+
 
   </>
   );
