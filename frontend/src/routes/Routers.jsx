61b61fe602ee94870c1a17e4f1b25e4d7c3aa01d
My Name <khyati0783.be21@chitkara.edu.in>
2024-03-20 12:02:05 +0530

    next commit

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -5,7 +5,7 @@ import Signup from "../pages/Signup";
 import Contact from "../pages/Contact";
 import Doctors from "../pages/Doctors/Doctors";
 import DoctorDetails from "../pages/Doctors/DoctorDetails";
-
+import MyAccount from "../Dashboard/user-account/MyAccount";
 import {Routes, Route} from 'react-router-dom';
 
 
@@ -20,6 +20,7 @@ const Routers = () => {
     <Route path="/register" element={<Signup/>} />
     <Route path="/contact" element={<Contact/>} />
     <Route path="/services" element={<Services/>} />
+    <Route path="/users/profile/me" element={<MyAccount/>} />
 
   </Routes>
   );
