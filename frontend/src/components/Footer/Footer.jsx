06d6349ff21eed087a3a2b508ab2f42d13d365e2
My Name <khyati0783.be21@chitkara.edu.in>
2024-04-09 01:12:56 +0530

    final commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/logo2.png';
 import {AiFillYoutube, AiFillGithub, AiFillInstagram, AiFillLinkedin} from 'react-icons/ai';
 
 const socialLinks = [
